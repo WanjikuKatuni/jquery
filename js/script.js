9d2c650d4a77d890f6d731ead73c36df34395fb4
wanjiku <annritawanjiku77@gmail.com>
2022-03-11 15:18:21 +0300

    remove SRI checking

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-const jquery360 = require("./jquery-3.6.0")
-
-
-
-jquery360(h1).click(function(){
-    alert('This is a header')
-})
-jquery360(p).click(function(){
-    alert('This is a paragraph')
-    alert('I told you this is a paragraph')
-})
-jquery360(img).click(function(){
-    alert('This is how I look')
-    alert("That's me")
-})
+jQuery("h1").click(function () {
+  alert("This is a header");
+});
+jQuery("p").click(function () {
+  alert("This is a paragraph");
+  alert("I told you this is a paragraph");
+});
+jQuery("img").click(function () {
+  alert("This is how I look");
+  alert("That's me");
+});
