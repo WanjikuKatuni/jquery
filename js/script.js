74282f4c11cf48f952cf0ca61312bfe3cc8a8ee6
wanjiku <annritawanjiku77@gmail.com>
2022-03-11 14:15:33 +0300

    add click and alert

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuerry(h1).click(function(){
+    alert('This is a header')
+})
+jQuerry(p).click(function(){
+    alert('This is a paragraph')
+    alert('I told you this is a paragraph')
+})
+jQuerry(img).click(function(){
+    alert('This is how I look')
+    alert("That's me")
+})
