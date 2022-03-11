74312e736660644741fe7f91895e8752710ee081
wanjiku <annritawanjiku77@gmail.com>
2022-03-11 14:18:55 +0300

    add click and alert

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-jQuerry(h1).click(function(){
+const jquery360 = require("./jquery-3.6.0")
+
+
+
+jquery360(h1).click(function(){
     alert('This is a header')
 })
-jQuerry(p).click(function(){
+jquery360(p).click(function(){
     alert('This is a paragraph')
     alert('I told you this is a paragraph')
 })
-jQuerry(img).click(function(){
+jquery360(img).click(function(){
     alert('This is how I look')
     alert("That's me")
 })
