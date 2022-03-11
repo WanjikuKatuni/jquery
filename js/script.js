40286475f75c9ac4f447d54280e0e45361ae101b
wanjiku <annritawanjiku77@gmail.com>
2022-03-11 15:21:37 +0300

    make jquery run after page has loaded

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-jQuery("h1").click(function () {
-  alert("This is a header");
-});
-jQuery("p").click(function () {
-  alert("This is a paragraph");
-  alert("I told you this is a paragraph");
-});
-jQuery("img").click(function () {
-  alert("This is how I look");
-  alert("That's me");
+//to makes sure the jquery waits for the page to load so that it can run
+jQuery(document).ready(function () {
+  //jquery function
+  jQuery("h1").click(function () {
+    alert("This is a header");
+  });
+  jQuery("p").click(function () {
+    alert("This is a paragraph");
+    alert("I told you this is a paragraph");
+  });
+  jQuery("img").click(function () {
+    alert("This is how I look");
+    alert("That's me");
+  });
 });
