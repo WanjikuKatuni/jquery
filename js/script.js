c35a3319b029ee9fcee35731be18012a82894578
wanjiku <annritawanjiku77@gmail.com>
2022-03-11 15:26:33 +0300

    using dollar signinstead of 'jquery'

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-//to makes sure the jquery waits for the page to load so that it can run
-jQuery(document).ready(function () {
-  //jquery function
-  jQuery("h1").click(function () {
-    alert("This is a header");
-  });
-  jQuery("p").click(function () {
-    alert("This is a paragraph");
-    alert("I told you this is a paragraph");
-  });
-  jQuery("img").click(function () {
-    alert("This is how I look");
-    alert("That's me");
-  });
-});
+// //to makes sure the jquery waits for the page to load so that it can run
+// jQuery(document).ready(function () {
+//   //jquery function
+//   jQuery("h1").click(function () {
+//     alert("This is a header");
+//   });
+//   jQuery("p").click(function () {
+//     alert("This is a paragraph");
+//     alert("I told you this is a paragraph");
+//   });
+//   jQuery("img").click(function () {
+//     alert("This is how I look");
+//     alert("That's me");
+//   });
+// });
+
+
+$(document).ready(function(){
+    $('h1').click(function(){
+        alert('the header')
+    })
+    $('p').click(fucntion(){
+        alert('the paragraph')
+    })
+    $('img').click(function(){
+        alert('the image')
+        alert('beautiful image')
+    })
+
+})
